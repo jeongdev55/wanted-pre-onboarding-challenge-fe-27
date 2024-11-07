e3bd2338d88f042007f6c5f2d18d8b39a03d09cb
jhj <wkdgowjdpo@gmail.com>
2024-11-07 23:48:43 +0900

    feat : 회원가입 폼 생성, 버튼 컴포넌트 생성

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,40 +1,29 @@
-import { useState } from "react";
 import { signup } from "../apis/authApi.ts";
 import { useNavigate } from "react-router-dom";
-import AuthForm from "@/components/auth/AuthForm.tsx";
+import AuthForm from "../components/auth/AuthForm.tsx";
+import {FieldValues} from "react-hook-form";
+
 
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const hendleSignupButtonClick = async () => {
-    try {
-      await signup({ email, password });
-      alert("회원가입 되었습니다. 로그인을 해주세요");
-      navigate("/login");
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  return (
-    <div>
-      <h1>Sign Up</h1>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Eamil"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="password"
-      />
-      <button onClick={hendleSignupButtonClick}>SignUp</button>
-    </div>
-  );
+    const handleSignupSubmit = async (data: FieldValues) => {
+        const {email, password} = data as { email: string; password: string };
+        try {
+            await signup({email, password});
+            alert("회원가입 되었습니다. 로그인을 해주세요");
+            navigate("/login");
+        } catch (err) {
+            console.error(err);
+        }
+    };
+    return (
+        <>
+        <div>
+            <AuthForm onSubmit={handleSignupSubmit} submitText="Sign Up"/>
+        </div>
+        </>
+    );
 };
 
 export default Signup;
